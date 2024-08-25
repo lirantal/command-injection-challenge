3fd49917e15e5633c5f08ad3194b4eaebb01cac5
Liran Tal <liran.tal@gmail.com>
2024-08-25 23:24:51 +0300

    feat: allow .env for sample app
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- src/templates/default/server/server.js
+++ src/templates/default/server/server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/api/timezone', (req, res) => {
-  console.log(req.query);
   res.json({ timezone: '1234' });
 });
 
